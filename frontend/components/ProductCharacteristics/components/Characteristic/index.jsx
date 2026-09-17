f2973ab764bdd1091fc4cf78d981cc563aae40ce
aylinuenal <aylin.uenal@shopgate.com>
2026-09-17 15:34:00 +0200

    Revert "Revert "CURB-6281 convert to new theme, clean up""
    This reverts commit 597fe14042555abee3f617299c2dcebcc3bb6228.

--- frontend/components/ProductCharacteristics/components/Characteristic/index.jsx
+++ frontend/components/ProductCharacteristics/components/Characteristic/index.jsx
@@ -1,86 +1,70 @@
 import React, { useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import find from 'lodash/find';
-import { css } from 'glamor';
-import classNames from 'classnames';
 import Transition from 'react-transition-group/Transition';
 import { I18n } from '@shopgate/engage/components';
-import { themeConfig } from '@shopgate/pwa-common/helpers/config';
+import { makeStyles } from '@shopgate/engage/styles';
 import CharacteristicValue from './components/CharacteristicValue';
 import CharacteristicValues from './components/CharacteristicValues';
 import { useProductCharacteristics } from '../../hooks';
-import config from '../../../../config';
-
-const { colors } = themeConfig;
+import config from '../../../../config.json';
 
 const {
   horizontalInsets,
   characteristicBorderColor,
-  showTrailingBorder,
+  showBottomBorder,
   showVariantPrices,
   variantSelectionAlwaysOpen,
 } = config;
 
 const insets = horizontalInsets || 0;
-const borderColor = characteristicBorderColor || '#C6C6C6';
 
-const styles = {
-  root: css({
+const useStyles = makeStyles()(theme => ({
+  root: {
     paddingLeft: insets,
     paddingRight: insets,
     transition: 'background 250ms ease-in, color 250ms ease-in',
     cursor: 'pointer',
-  }).toString(),
-  disabled: css({
-    color: colors.shade4,
+  },
+  disabled: {
+    color: theme.palette.text.disabled,
     cursor: 'default !important',
-  }),
-  container: css({
+  },
+  container: {
     borderTop: '1px solid',
-    borderTopColor: borderColor,
+    borderTopColor: characteristicBorderColor || theme.components.border.medium,
     display: 'flex',
     flexDirection: 'column',
     padding: '8px 0',
-  }).toString(),
-  containerLast: css({
-    ...(showTrailingBorder ? {
+  },
+  containerLast: {
+    ...(showBottomBorder ? {
       borderBottom: '1px solid',
-      borderBottomColor: borderColor,
+      borderBottomColor: characteristicBorderColor || theme.components.border.medium,
     } : null),
-  }).toString(),
-  characteristic: css({
+  },
+  characteristic: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingLeft: insets ? 0 : 16,
     paddingRight: insets ? 0 : 16,
-  }),
-  label: css({
+  },
+  label: {
     display: 'flex',
     fontWeight: 'bold',
     paddingTop: 8,
     paddingBottom: 8,
     paddingRight: 8,
     textAlign: 'left',
-  }),
-  value: css({
+  },
+  value: {
     display: 'flex',
     justifyContent: 'flex-end',
     paddingLeft: 4,
     textAlign: 'right',
-  }),
-};
-
-const transition = {
-  entering: {
-    background: colors.primary,
-    color: colors.primaryContrast,
   },
-  entered: {
-    background: colors.primary,
-    color: colors.primaryContrast,
-  },
-};
+}));
 
 /**
  * Characteristic component
@@ -97,12 +81,24 @@ const Characteristic = ({
   values,
   resetHighlight,
 }) => {
+  const { classes, cx, theme } = useStyles();
   const {
     characteristicStates,
     setOpenState,
     allowMultipleOpen,
     productVariants,
   } = useProductCharacteristics();
+
+  const transition = {
+    entering: {
+      background: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+    },
+    entered: {
+      background: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+    },
+  };
 
   // Determine the states for the current characteristic
   const { isOpen, isLast } = useMemo(
@@ -159,24 +155,22 @@ const Characteristic = ({
     <Transition in={highlight} timeout={500} onEntered={resetHighlight}>
       { state => (
         <div
-          className={
-          classNames(styles.root, 'pdp-variant-accordion__characteristic', {
-            disabled,
-          })}
+          className={cx(classes.root, 'pdp-variant-accordion__characteristic', { disabled })}
           ref={charRef}
           style={transition[state]}
         >
           <div
-            className={classNames(
-              styles.container,
-              'pdp-variant-accordion__characteristic__header', {
-                [styles.containerLast]: isLast,
-                [styles.disabled]: disabled,
+            className={cx(
+              classes.container,
+              'pdp-variant-accordion__characteristic__header',
+              {
+                [classes.containerLast]: isLast,
+                [classes.disabled]: disabled,
               }
             )}
           >
             <div
-              className={styles.characteristic}
+              className={classes.characteristic}
               onClick={handleClick}
               onKeyDown={() => {}}
               role="button"
@@ -184,10 +178,10 @@ const Characteristic = ({
               aria-disabled={disabled}
               tabIndex="-1"
             >
-              <div className={styles.label}>
+              <div className={classes.label}>
                 {label}
               </div>
-              <div className={styles.value}>
+              <div className={classes.value}>
                 { selectedValue ? (
                   <CharacteristicValue
                     characteristicId={id}
